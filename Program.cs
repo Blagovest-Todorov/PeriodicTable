2db3576aebcb5cb9b44f0cbe4acf915f6c32e8f5
Blagovest-Todorov <blagovest.stod@abv.bg>
2021-05-31 08:22:27 +0300

    add Program.cs

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PeriodicTable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SortedSet<string> elements = new SortedSet<string>();
+           // string[] arr = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            int inputLines = int.Parse(Console.ReadLine());
+
+            if (inputLines <= 0)
+            {
+                Environment.Exit(1);
+            }
+
+            FillSortedSet(elements, inputLines);
+
+            if (elements.Count != 0)
+            {
+                PrintSortedSet(elements);
+            }   
+        }
+
+        private static void PrintSortedSet(SortedSet<string> elements)
+        {
+            Console.WriteLine(string.Join(" ", elements ));
+        }
+
+        private static void FillSortedSet(SortedSet<string> elements, int numberLines)
+        {
+            for (int i = 0; i < numberLines; i++)
+            {
+                string[] line = Console.ReadLine()
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+                if (line.Length <= 0)
+                {
+                    continue;
+                }
+
+                foreach (var item in line)
+                {
+                    elements.Add(item);
+                }
+            }
+        }
+    }
+}
